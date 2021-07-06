c9fd05e4e7f27a59f05a5ea88305694ec7e3a43e
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-05 19:32:13 -0700

    added criteria

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@
 
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
+let origin = "";
 let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let upperCase = "ABCDEFGHIJKLMMOPQRSTUVWXYZ";
 let number = "0123456789";
@@ -13,11 +14,17 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
   let length = window.prompt("Please enter the password length between 8 and 128");
   if ((length >= 8) && (length <= 128)) {
+    let confirmLower = window.confirm('Do you want to include lower case letters in your password?');
     let confirmUpper = window.confirm('Do you want to include upper case letters in your password?');
     let confirmNumber = window.confirm('Do you want to include numbers in your password?');
     let confirmSymbol = window.confirm('Do you want to include symbols in your password?');
 
-    let characters = lowerCase;
+    if ((!confirmLower) && (!confirmUpper) && (!confirmNumber) && (!confirmSymbol)){
+      alert("Please select at least one type.")
+    }
+    else{
+    let characters = origin;
+    confirmLower ? (characters +=lowerCase) :"";
     confirmUpper ? (characters += upperCase) : "";
     confirmNumber ? (characters += number) : "";
     confirmSymbol ? (characters += symbol) : "";
@@ -34,7 +41,7 @@ function writePassword() {
     }
   console.log(generatePassword);
   passwordText.value = generatePassword(length, characters);
-} else {
+}} else {
       window.alert("Password must be between 8 and 128 digits long.")
   };
 }
