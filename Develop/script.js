a16e14b9ec81548d500b425c21e74b26d171df10
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-05 19:23:01 -0700

    added javascript

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,46 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMMOPQRSTUVWXYZ";
+let number = "0123456789";
+let symbol = "!@#$%^&*()_+=";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let passwordText = document.querySelector("#password");
+  let length = window.prompt("Please enter the password length between 8 and 128");
+  if ((length >= 8) && (length <= 128)) {
+    let confirmUpper = window.confirm('Do you want to include upper case letters in your password?');
+    let confirmNumber = window.confirm('Do you want to include numbers in your password?');
+    let confirmSymbol = window.confirm('Do you want to include symbols in your password?');
 
-  passwordText.value = password;
+    let characters = lowerCase;
+    confirmUpper ? (characters += upperCase) : "";
+    confirmNumber ? (characters += number) : "";
+    confirmSymbol ? (characters += symbol) : "";
+    
 
+    let generatePassword = (length, characters) => {
+      let password = "";
+      for (let i = 0; i < length; i++) {
+        password += characters.charAt(
+          Math.floor(Math.random() * characters.length)
+        );
+      }
+      return password;
+    }
+  console.log(generatePassword);
+  passwordText.value = generatePassword(length, characters);
+} else {
+      window.alert("Password must be between 8 and 128 digits long.")
+  };
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
